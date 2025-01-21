c803fb6927d13c288164064e823c93931ec0e9cc
Brad Akemi <mirado.akemi@gmail.com>
2025-01-21 21:51:24 +0300

    index

---
+++ pages/index.tsx
@@ -0,0 +1,51 @@
+import type { GetServerSideProps, InferGetStaticPropsType } from 'next'
+import { useRouter } from 'next/router'
+
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import Footer from './components/footer'
+import HomePage from './components/home'
+import MatrixRain from './components/matrix-rain'
+import Navigation from './components/navigation'
+
+
+
+type Props = {
+  // Add custom props here
+}
+
+const Homepage = (
+  _props: InferGetStaticPropsType<typeof getServerSideProps>
+) => {
+  const router = useRouter()
+  const { t, i18n } = useTranslation('common')
+  // const toggleLanguage = () => {
+  //   const newLocale = i18n.resolvedLanguage === 'en' ? 'fr' : 'en'
+  //   i18n.changeLanguage(newLocale)
+  //   router.push(router.pathname, router.asPath, { locale: newLocale })
+  // }
+
+  return (
+    <div className="min-h-screen bg-black text-white overflow-hidden relative">
+      <MatrixRain />
+      <div className="relative z-10">
+        <Navigation />
+        <HomePage />
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+// or getServerSideProps: GetServerSideProps<Props> = async ({ locale })
+export const getServerSideProps: GetServerSideProps<Props> = async ({
+  locale,
+}) => ({
+  props: {
+    ...(await serverSideTranslations(locale ?? 'en', [
+      'common',
+    ])),
+  },
+})
+
+export default Homepage
