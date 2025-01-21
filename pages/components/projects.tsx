cb9e78de2fc4637d04b10240e5f764758deb9dfa
Brad Akemi <mirado.akemi@gmail.com>
2025-01-21 21:52:55 +0300

    proj

--- pages/components/projects.tsx
+++ pages/components/projects.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "../../components/ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../../components/ui/card"
 import { motion } from "framer-motion"
 import { ArrowRight } from "lucide-react"
 import Link from "next/link"
